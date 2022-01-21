86e4a34c05af3cb17d04b464267911586b35aaad
nosfsos <nosferatus.sos@gmail.com>
2022-01-21 18:20:39 +0000

    fist commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace ScriptDebugger
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.open = new System.Windows.Forms.Button();
+            this.addDebugLines = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // open
+            // 
+            this.open.Location = new System.Drawing.Point(172, 58);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(75, 23);
+            this.open.TabIndex = 0;
+            this.open.Text = "Open";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // addDebugLines
+            // 
+            this.addDebugLines.Location = new System.Drawing.Point(122, 132);
+            this.addDebugLines.Name = "addDebugLines";
+            this.addDebugLines.Size = new System.Drawing.Size(177, 73);
+            this.addDebugLines.TabIndex = 1;
+            this.addDebugLines.Text = "Add Debug Lines";
+            this.addDebugLines.UseVisualStyleBackColor = true;
+            this.addDebugLines.Click += new System.EventHandler(this.addDebugLines_Click);
+            // 
+            // mainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 290);
+            this.Controls.Add(this.addDebugLines);
+            this.Controls.Add(this.open);
+            this.Name = "MainWindow";
+            this.Text = "Debugger";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.Button addDebugLines;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScriptDebugger
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private List<string> _filePaths;
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                var result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    _filePaths = Directory.EnumerateFiles(fbd.SelectedPath, "*.psc", SearchOption.AllDirectories).ToList();
+
+                    MessageBox.Show(@"Files found: " + _filePaths.Count, @"Message");
+
+                    //MessageBox.Show(_filePaths[0]);
+                }
+            }
+        }
+
+        private void addDebugLines_Click(object sender, EventArgs e)
+        {
+            var startDebug = "Debug.notification(\"Function starting!\")";
+            var endDebug = "Debug.notification(\"Function ending!\")";
+
+            foreach (var filePath in _filePaths)
+            {
+                //var readText = File.ReadAllLines(filePath).ToList();
+
+                var fileText = new List<string>();
+                using (var reader = new StreamReader(filePath))
+                {
+                    while (reader.EndOfStream == false)
+                    {
+                        var line = reader.ReadLine();
+
+                        if (line.Length == 0 || line[0] == ';') // current line is empty or is a comment
+                            continue;
+
+                        if (line.Contains("Function") && line.Contains("(") && line.Contains(")")) // function declaration line
+                        {
+                            // add startDebug line
+
+                            var spacesCount = Regex.Matches(line, "\t");
+                            var sb = new StringBuilder();
+
+                            for (var i = 0; i < spacesCount.Count; i++)
+                            {
+                                sb.Append("\t");
+                            }
+
+                            sb.Append("\t"); // extra indent to be ahead of Function
+
+                            fileText.Add(line);
+                            fileText.Add(sb + startDebug);
+                        }
+                        else if (line.Contains("EndFunction") || line.Contains("return"))
+                        {
+                            // add endDebug before returns and EndFunctions
+
+                            var spacesCount = Regex.Matches(line, "\t");
+                            var sb = new StringBuilder();
+
+                            for (var i = 0; i < spacesCount.Count; i++)
+                            {
+                                sb.Append("\t");
+                            }
+
+                            if (line.Contains("EndFunction"))
+                                sb.Append("\t"); // extra indent to be ahead of EndFunction. Not needed in case of return
+
+                            fileText.Add(sb + endDebug);
+                            fileText.Add(line);
+                        }
+                        else
+                        {
+                            fileText.Add(line);
+                        }
+                    }
+                    // write the lines back to the file
+                    reader.Dispose();
+                }
+
+                File.WriteAllLines(filePath, fileText);
+
+                //readText.Clear();
+            }
+
+            MessageBox.Show(@"Done");
+        }
+    }
+}
